c52120dba92591efaa8d970257ae1eb192cf642c
EliasDev <eliasrmartinez00@gmail.com>
2026-07-08 20:12:20 -0600

    actualiza eliminar.php

--- eliminar.php
+++ eliminar.php
@@ -1,23 +1,23 @@
-<?php 
+<?php
+    // recibimos el ID a eliminar desde la URL
     $idAEliminar = $_GET['id'];
     $archivo = 'usuarios.csv';
 
-    // recorremos el archivo y nos quedamos solo con las lines que no coincidan
+    // recorremos el archivo y nos quedamos solo con las líneas que NO coincidan
     $lineas = file($archivo, FILE_IGNORE_NEW_LINES);
     $contenidoNuevo = "";
 
-    foreach ($lineas as linea) {
+    foreach ($lineas as $linea) {
         $datos = explode(",", $linea);
         $idActual = $datos[0];
 
-        if ($idActual != $idAEliminar ) {
+        if ($idActual != $idAEliminar) {
             $contenidoNuevo = $contenidoNuevo . $linea . "\n";
         }
-    
-}
+    }
 
-        file_put_contents($archivo, $contenidoNuevo);
-        header("Location: lista.php");
-        exit;
-
-        ?>
+    // sobrescribimos el archivo sin la línea eliminada y volvemos a la lista
+    file_put_contents($archivo, $contenidoNuevo);
+    header("Location: lista.php");
+    exit;
+?>
